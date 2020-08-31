c07d0b6c257788be58e41def3ae26950fbdf269e
heathermortensen <mortensenheather@hotmail.com>
2020-08-31 16:24:20 -0700

    front end accepts user input

--- piggy.py
+++ piggy.py
@@ -1,4 +1,4 @@
-# from tkinter import *
+from tkinter import *
 import tkinter
 from PIL import Image, ImageTk
 # The next 2 imports override the basic Tk widgets
@@ -19,12 +19,12 @@ def main():
     root = tkinter.Tk()
     window1 = Window(root, "Play_a_game_of_Pig", "969x690", "someMessage",
                      "pig.ico", 0)
-
     return None
 
 
 class Window:
     number_of_players = 0
+    # int player_names[] = new player_names[number_of_players]
 
     def __init__(self, root, title, geometry, message, iconbitmap,
                  playerNumber):
@@ -34,23 +34,43 @@ class Window:
         self.root.geometry(geometry)
         self.root.iconbitmap(iconbitmap)
         self.number_of_players = playerNumber
+        # Make the window show up in center of screen and not be ccovered up by anything else.
+        # self.root.eval('tk::PlaceWindow %s center' % self.root.wininfo_toplevel())
+        # this is an important line.
+        # self.root.mainloop()
 
-        def addPlayers(self):
+        def addPlayers():
             """Allows user to input the number of and names of all game players"""
             # stackoverflow.com/questions/12169258/should-i-use-entrys-get-or-its-textvariables-for-tkinter-in-python
 
-            print("/n initial # of players = " + str(self.number_of_players))
+            print("\n initial # of players = " + str(self.number_of_players))
 
             # Collects user input from the entry and turns it into an int
-            user_input_number_of_players.set(int(str(entry_player_number.get("1.0", 'end-1c'))))
+            # user_input_number_of_players.set(int(str(entry_player_number.get("1.0", 'end-1c'))))
+            try:
+                user_input_number_of_players = int(entry_player_number.get())
+                print("Inside try block, user_input = ")
+                print(user_input_number_of_players)
+                self.number_of_players = user_input_number_of_players
+
+            except ValueError:
+                tkinter.messagebox.showerror('Non-Integer Input', 'User MUST enter a player # greater than 1.', icon = 'error')
+                # tkinter.messagebox.deiconify()
+                # tkinter.messagebox.quit()
+                # tkinter.messagebox.destroy()
+                
+            
+            #user_input_number_of_players.set(int(str(entry_player_number.get("1.0", 'end-1c'))))
 
             # Set class instance value to this input from the user
-            self.number_of_players = user_input_number_of_players
+            # self.number_of_players = user_input_number_of_players
 
             print("# of players after click = " + str(self.number_of_players))
 
-        return None
-
+            return self.number_of_players
+        
+        print("# of players after click = " + str(self.number_of_players))
+        
         # Add a label
 
         myLabel1 = tkinter.Label(self.root, text="Please Enter # of Players",
@@ -80,7 +100,6 @@ class Window:
 
         addPlayerButton.grid(row=2, column=4)
 
-        # this is an important line.
         self.root.mainloop()
 
         pass
